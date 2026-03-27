34b28e2803f2d84addd6f540d8b7ee23b8229a12
Sven Schrodt <sven@schrodt.club>
2026-03-27 14:10:35 +0100

    Adding stuff ..

--- foo.php
+++ foo.php
@@ -1,4 +1,8 @@
 <?php
 
+use UnitCheck\Shell\Cli;
 
 var_dump(new \UnitCheck\Entity\Cfg('unitcheck.ini'));
+
+$cli = new Cli();
+echo $cli->getColouredString('Fool me not ypu fool!', 'red', 'blue');
